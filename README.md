74ed12f0ca710d78888769950f8b70765ae16b76
Eugene <t-yijunc@microsoft.com>
2020-07-10 07:25:30 +0000

    added readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# azure-storage-gst-plugin
+
+This is a gstreamer sink for azure storage.
+
+## Prerequisites
+
+You'll need `azure-storage-cpplite` and `gstreamer`.
+
+* Follow [azure-storage-cpplite](https://github.com/Azure/azure-storage-cpplite)'s readme and install it locally.
+* [Install gstreamer](https://gstreamer.freedesktop.org/documentation/installing/index.html?gi-language=c#) on your platform.
+
+## Build
+
+This project use cmake to build.
+
+```bash
+mkdir build
+cd build
+cmake ..
+make # and then run...
+```
+
+# Using the plugin
+
+TODO :)
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <memory>
 
 // #include <gstreamer-1.0/gst/gst.h>
@@ -17,17 +18,23 @@ int main(int argc, char** argv) {
     std::string account_name = "gstvideostore";
     std::string account_key = "vIjLNYmW60yVH1nLc08u/KGJNXZl6Gzy7zdGtpSK3JJ6vSQfjRQUB8z/UpEoS27J4Tkl1/a30blvTPurkdC3jA==";
     bool use_https = true;
-    std::string blob_endpoint = "";
-    int connection_count = 2;
 
     // Setup the client
     auto credential = make_shared<azure::storage_lite::shared_key_credential>(account_name, account_key);
-    auto storage_account = make_shared<azure::storage_lite::storage_account>(account_name, credential, use_https, blob_endpoint);
-    azure::storage_lite::blob_client client(storage_account, connection_count);
+    auto storage_account = make_shared<azure::storage_lite::storage_account>(account_name, credential, use_https);
+    azure::storage_lite::blob_client client(storage_account, 16);
     // Start using
-    auto result = client.append_block_from_stream("videostore", "teststream", std::cin);
+    std::cout << "Creating test stream..." << std::endl;
+    auto result = client.create_append_blob("videostore", "teststream");
     result.wait();
     auto r = result.get();
+    handle(r);
+    std::cout << "Appending to append block from sstream..." << std::endl;
+    std::string content = "This is the content being appended.";
+    std::istringstream iss(content);
+    result = client.append_block_from_stream("videostore", "teststream", iss);
+    result.wait();
+    r = result.get();
     handle(r);
     return 0;
 }
